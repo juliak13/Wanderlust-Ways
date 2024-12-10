8790f5db5404c6d3913231f8766412d3e559bde8
Julia Konopka <juliakonopkausa@gmail.com>
2024-12-10 18:18:51 -0500

    Initial Commit

---
+++ WanderlustWays/WanderlustWaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  WanderlustWaysApp.swift
+//  WanderlustWays
+//
+//  Created by Julia Konopka on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WanderlustWaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
